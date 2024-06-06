aa478e70a4f859b0c14c811fa32eea60786d94bc
LittleAtariXE <bewube@o2.pl>
2024-06-06 17:20:40 +0200

    few correction

--- app/tools/cracker.py
+++ app/tools/cracker.py
@@ -1,4 +1,5 @@
 import hashlib
+import time
 from threading import Thread
 from termcolor import cprint
 from multiprocessing import Process, Lock
@@ -20,31 +21,34 @@ class Cracker(Process):
         self.out_file = hc_callback.config.file_default_out
         self.lock = Lock()
         self.password = None
+        self.done_time = "unknown"
     
     def crackDict(self) -> None:
+        start_time = time.time()
         for p in self.pass_list:
             hashed = self.method(p).hexdigest()
-            # cprint(f"Try password: {p}", "yellow")
             if hashed == self.hash_pass:
                 p = p.decode(self.encode_format)
                 cprint(f"PASSWORD FINDED !! Found {self.__method} password: {p} ", "green")
                 self.password = p
                 break
+        self.done_time = time.time() - start_time
         self.report()
     
     def report(self) -> None:
         report = "*" * 80
         if not self.password:
             report += "\nPassword not finded\n"
-            if self.login:
-                report += f"Login: {self.login}\n"
-            report += f"Hash {self.__method}: {self.hash_pass}\n"
         else:
             report += "\nPaswword finded !!!!!\n"
-            if self.login:
-                report += f"Login: {self.login}\n"
-            report += f"Hash {self.__method}: {self.hash_pass}\n"
             report += f"PASSWORD: {self.password}\n"
+        if self.login:
+            report += f"Login: {self.login}\n"
+        else:
+            report += "NO LOGIN\n"
+        report += f"Hash {self.__method}: {self.hash_pass}\n"
+        report += f"Working Time: {round(self.done_time, 2)} sec.\n"
+        report += f"Number of passwords: {len(self.pass_list)}\n"
         report += "*" * 80 + "\n"
         with self.lock:
             with open(self.out_file, "a+") as file:
